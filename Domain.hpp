d768e59466164e8ed4fa6f66c645f3c374a38553
Eike Lage <elage@ethz.ch>
2022-11-18 12:01:25 +0100

    changes to grid generation

--- Domain.hpp
+++ Domain.hpp
@@ -8,7 +8,7 @@ class Domain{
 		Domain(const Domain&); //Copy constructor
 		Domain& operator =(Domain&);
         Point Domain::operator()(int i, int j) const;
-		void generate_grid(int, int);
+		void generate_grid(int, int, int); // pass mxn grid and interpolation option. 1-linear, 2-whateverthatoptionalthingis
 		// ...
 	private:
 		Curvebase *sides[4];
@@ -16,6 +16,8 @@ class Domain{
         double *x_ = nullptr, *y_ = nullptr;
         int n_ = 0, m_ = 0;
 		bool check_consistency(); //Check if the grid is topologically equivalent to a square
+		double phi1(double); //transition function for grid generation phix(0) = 1, phix(1) = 0
+		double phi2(double); //transition function for grid generation phiy(1) = 1, phix(0) = 0
 		// ...
 };
 #include "Domain.cpp"
